650e7c8792a9cc70a896ea5e460b0a3e4ec55041
jvital17 <116577184+jvital17@users.noreply.github.com>
2022-10-24 10:54:49 -0500

    Update streamlit_app.py
    added filtering to fruits table

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,10 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# interactive pick list, with prepopulated items
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+# interactive pick list, with prepopulated items. Storing selections in fruits_selected and using this to filter fruit_list df
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display on app
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
