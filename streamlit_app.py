119264047fde2e09806df45135e3d4eaf250d5e8
jvital17 <116577184+jvital17@users.noreply.github.com>
2022-10-24 10:14:25 -0500

    Update streamlit_app.py
    imported fruit macros

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# importing fruit macros
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# display on app
+streamlit.dataframe(my_fruit_list)
