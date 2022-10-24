149f96360a4fc470ad809154877a29f421331036
jvital17 <116577184+jvital17@users.noreply.github.com>
2022-10-24 10:49:29 -0500

    Update streamlit_app.py
    added prepopulated filtering - Avocado & Strawberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# interactive pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# interactive pick list, with prepopulated items
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 # display on app
 streamlit.dataframe(my_fruit_list)
